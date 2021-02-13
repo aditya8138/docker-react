ff241340e2d47479e02bab3d7864380a48423e44
Aditya <aditya8138@gmail.com>
2021-02-12 17:14:08 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+#Build Step
+
+FROM node:alpine
+
+WORKDIR '/app'
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+
+#Run Step
+FROM nginx
+# copy build folder to nginx
+COPY --from=0 /app/build /usr/share/nginx/html
+#nginx container will start nginx itself we don't need to give a command for it to start
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,16 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Yeah baby', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react Application', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
