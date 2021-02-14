763e4e2f5b23f372e32428f6310b14a328746cc7
Aditya <aditya8138@gmail.com>
2021-02-13 21:56:37 -0800

    Change App description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          There, There!
+          This is the prod version of the application.
         </p>
         <a
           className="App-link"
